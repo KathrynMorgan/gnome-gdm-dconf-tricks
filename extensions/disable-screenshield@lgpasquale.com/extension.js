619995bf62784576ae2aa67ae098c28531eb8120
katamo <kat.morgan@braincraft.io>
2019-02-19 17:32:59 +0000

    initial refrence/experiments

---
+++ extensions/disable-screenshield@lgpasquale.com/extension.js
@@ -0,0 +1,27 @@
+const ScreenShield = imports.ui.screenShield;
+
+let _onUserBecameActiveOrig;
+
+function _onUserBecameActiveInjected()
+{
+	this._liftShield(true /* onPrimary */, 0 /* velocity */);
+	_onUserBecameActiveOrig.call(this)
+}
+
+function init()
+{
+}
+
+function enable()
+{
+    _onUserBecameActiveOrig =
+        ScreenShield.ScreenShield.prototype._onUserBecameActive;
+    ScreenShield.ScreenShield.prototype._onUserBecameActive =
+        _onUserBecameActiveInjected;
+}
+
+function disable()
+{
+    ScreenShield.ScreenShield.prototype._onUserBecameActive =
+        _onUserBecameActiveOrig;
+}
